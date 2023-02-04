fcce0eb29df8b157e09236ad6fc54196e92c219b
Nenkenne360 <111001012+Nenkenne360@users.noreply.github.com>
2023-02-04 04:22:43 +0000

    Update 0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -6,8 +6,11 @@ pattern = /School/
 # Get the argument passed to the script
 arg = ARGV[0]
 
+# Pass the argument to the regular expression matching method
+result = arg.match(pattern)
+
 # Check if the argument matches the pattern
-if arg =~ pattern
+if result
   puts "#{arg} matches the pattern #{pattern}"
 else
   puts "#{arg} does not match the pattern #{pattern}"
