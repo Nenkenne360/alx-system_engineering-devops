e2d3bab4fd1815ab9c3e6f9c4130e1f96605c5fb
Nenkenne360 <111001012+Nenkenne360@users.noreply.github.com>
2023-02-04 04:24:40 +0000

    Update 0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,17 +1,2 @@
 #!/usr/bin/env ruby
-
-# Define the regular expression pattern to match 'School'
-pattern = /School/
-
-# Get the argument passed to the script
-arg = ARGV[0]
-
-# Pass the argument to the regular expression matching method
-result = arg.match(pattern)
-
-# Check if the argument matches the pattern
-if result
-  puts "#{arg} matches the pattern #{pattern}"
-else
-  puts "#{arg} does not match the pattern #{pattern}"
-end
+puts ARGV[0].scan(/Holberton/).join
